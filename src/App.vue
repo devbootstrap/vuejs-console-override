0a99f57d98be601a52006356148ed71c0ab9f92e
Darren Jensen <darren.jensen@gmail.com>
2020-02-26 17:50:32 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Console />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Console from './components/Console.vue';
+
+@Component({
+  components: {
+    Console,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Console.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Console output will appear below..</p>
+    <hr />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Console extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
